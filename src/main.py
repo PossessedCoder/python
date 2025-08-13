35b92e62233643c6ddb8390d2a74921ba0a85b41
PossessedCoder <109951949+PossessedCoder@users.noreply.github.com>
2025-08-13 16:49:27 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ def quote_post():
 @app.route('/post_quote', methods=['POST'])
 def add_quote():
     with open("templates/main_page.html", 'a') as f:
-        f.write(f'<h1>{request.form['PostText']}</h1>')
+        f.write(f'<h1>{request.form['PostText']}</h1>\n')
     return redirect('/')
 # def get_quotes():
 #     with open("main_page.html") as f:
