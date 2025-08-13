0a8c8d8c0fdebe92b8a3a13cfbf015353451b8a3
PossessedCoder <109951949+PossessedCoder@users.noreply.github.com>
2025-08-13 15:48:40 +0300

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('main_page.html')
+
+@app.route('/post')
+def quote_post():
+    return render_template('post_page.html')
+
+@app.route('/post_quote', methods=['POST'])
+def add_quote():
+    with open("templates/main_page.html", 'a') as f:
+        f.write(f'<h1>{request.form['PostText']}</h1>')
+    return redirect('/')
+# def get_quotes():
+#     with open("main_page.html") as f:
+#         return f.readlines()
+
