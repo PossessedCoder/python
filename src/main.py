bd8c9c007bcaeee40608a8e0ef4a6ccd8d4001c4
PossessedCoder <109951949+PossessedCoder@users.noreply.github.com>
2025-08-15 23:29:43 +0300

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -11,9 +11,11 @@ def quote_post():
 
 @app.route('/post_quote', methods=['POST'])
 def add_quote():
-    with open("templates/main_page.html", 'a') as f:
-        f.write(f'<h1>{request.form['PostText']}</h1>\n')
+    with open("templates/main_page.html", 'a', encoding='utf-8') as f:
+        f.write(f'<h1>{request.form['PostText']}. {request.form['Selector']}. Автор цитаты: {request.form['Author']}</h1>\n')
     return redirect('/')
+
+app.run(debug=True)
 # def get_quotes():
 #     with open("main_page.html") as f:
 #         return f.readlines()
